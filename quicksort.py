07a5bcc7c51c3ce97017d59040eaf859f35f582c
Trevor Dinh <thinhqd@uci.edu>
2018-11-30 16:35:11 -0800

    Refactor quicksort notebook into a class. Fixed quicksort standalone with better debug trace prints

--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,7 @@
 trace_debug = True
 import random
 
+
 def quicksort(arr, l, h):
     if h-l > 0:
         p = partition(arr, l, h)
@@ -11,23 +12,28 @@ def quicksort(arr, l, h):
 def partition(arr, l, h):
     p = h
     firsthigh = l
-    print("quicksort: pivot = arr[{}] ({}), l = arr[{}] ({}), h = arr[{}] ({})".format(p, arr[p], l, arr[l], h, arr[h]))
+    print("quicksort: arr = {}, pivot = arr[{}] ({}), l = arr[{}] ({}), h = arr[{}] ({})".format(arr[l:h+1],
+        p, arr[p], l, arr[l], h, arr[h]))
     for i in range(l, h):
         print("loop {}".format(i))
         if arr[i] < arr[p]:
             if trace_debug:
-                print("arr[{}] ({}) is less than arr[{}] ({})".format(i,arr[i], p, arr[p]))
-                print("In loop, swapping arr[{}] ({}) and arr[{}] ({})".format(i, arr[i], firsthigh, arr[firsthigh]))
+                print("arr[{}] ({}) is less than arr[{}] ({})".format(
+                    i, arr[i], p, arr[p]))
+                print("In loop, swapping arr[{}] ({}) and arr[{}] ({})".format(
+                    i, arr[i], firsthigh, arr[firsthigh]))
             arr[i], arr[firsthigh] = arr[firsthigh], arr[i]
             firsthigh += 1
+            if trace_debug:
+                print("firsthigh is now {}".format(firsthigh))
     arr[p], arr[firsthigh] = arr[firsthigh], arr[p]
-    print("arr at end of quicksort: {}".format(arr))
+    print("arr at end of quicksort call: {}".format(arr[l:h+1]))
     return firsthigh
 
 
 if __name__ == '__main__':
-    a = [i for i in range(30, 0, -1)]
-    random.shuffle(a)
+    a = [3, 7, 8, 5, 2, 1, 9, 5, 4]
+    # random.shuffle(a)
     print(a)
     quicksort(a, 0, len(a)-1)
     print(a)
